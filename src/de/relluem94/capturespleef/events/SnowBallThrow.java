d6a14ef35a15e36ca5a5e4ca638faa163a62bc95
Rellu <github@relluem94.de>
2020-12-19 03:45:45 +0100

    refactor

--- src/de/relluem94/capturespleef/events/SnowBallThrow.java
+++ src/de/relluem94/capturespleef/events/SnowBallThrow.java
@@ -1,4 +1,3 @@
-
 package de.relluem94.capturespleef.events;
 
 import java.util.Arrays;
@@ -19,37 +18,46 @@ import org.bukkit.util.Vector;
  * @author rellu
  */
 public class SnowBallThrow implements Listener {
-    
+
     de.relluem94.capturespleef.CaptureSpleef main;
 
     public SnowBallThrow(de.relluem94.capturespleef.CaptureSpleef instance) {
         main = instance;
     }
-    
+
     @EventHandler
     public void Schneeball(PlayerInteractEvent e) {
+        ItemStack snowballs = new ItemStack(Material.SNOWBALL, 94);
+        ItemMeta snowball_meta = snowballs.getItemMeta();
+        snowball_meta.setDisplayName(main.hubitem5);
+        snowball_meta.setLore(Arrays.asList(main.hubitem5_info_1, main.hubitem5_info_2, main.hubitem5_info_3, main.hubitem5_info_4));
+        snowballs.setItemMeta(snowball_meta);
+
         Player p = e.getPlayer();
         if (p.hasPermission("rellu.lobby.snowball")) {
-            ItemStack test = p.getInventory().getItemInHand();
-            if (test.getType() == Material.SNOWBALL) {
+            ItemStack itemInHand = p.getInventory().getItemInHand();
+            if (itemInHand.getType() == Material.SNOWBALL) {
                 if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_AIR) {
-                    ItemStack d1 = new ItemStack(Material.SNOWBALL, 94);
-                    ItemMeta d2 = d1.getItemMeta();
-                    d2.setDisplayName(main.hubitem5);
-                    d2.setLore(Arrays.asList(main.hubitem5_info_1, main.hubitem5_info_2, main.hubitem5_info_3, main.hubitem5_info_4));
-                    d1.setItemMeta(d2);
-                    p.getInventory().setItem(2, new ItemStack(d1));
-                    ItemMeta test2 = test.getItemMeta();
-                    String test3 = test2.getDisplayName();
-                    if (test3 == null) {
+
+                    p.getInventory().setItem(2, new ItemStack(snowballs));
+                    ItemMeta itemInHandMeta = itemInHand.getItemMeta();
+                    String itemInHandMetaDisplayname = itemInHandMeta.getDisplayName();
+                    if (itemInHandMetaDisplayname == null) {
                         return;
                     }
-                    if (test3.contains("Schneeball")) {
-                        Vector test231 = p.getEyeLocation().multiply(7.8).getDirection();
-                        Vector vec = new Vector(test231.getX(), test231.getY(), test231.getZ());
+                    if (itemInHandMetaDisplayname.contains("Schneeball")) {
+                        Vector vec_direction = p.getEyeLocation().multiply(7.8).getDirection();
+                        Vector vec = new Vector(vec_direction.getX(), vec_direction.getY(), vec_direction.getZ());
                         Projectile ball = p.launchProjectile(Snowball.class);
                         ball.setVelocity(vec);
-                        if (p.hasPermission("rellu.group.vip") || p.hasPermission("rellu.group.builder") || p.hasPermission("rellu.group.sgbuilder") || p.hasPermission("rellu.group.pMod") || p.hasPermission("rellu.group.Mod") || p.hasPermission("rellu.group.sMod") || p.hasPermission("rellu.group.Geek") || p.hasPermission("rellu.group.Admin")) {
+                        if (p.hasPermission("rellu.group.vip")
+                                || p.hasPermission("rellu.group.builder")
+                                || p.hasPermission("rellu.group.sgbuilder")
+                                || p.hasPermission("rellu.group.pMod")
+                                || p.hasPermission("rellu.group.Mod")
+                                || p.hasPermission("rellu.group.sMod")
+                                || p.hasPermission("rellu.group.Geek")
+                                || p.hasPermission("rellu.group.Admin")) {
                             if (p.getName().equals("Relluem94")) {
                                 main.shuffle();
                                 ball.setCustomName(main.setColors().get(2) + "R" + main.setColors().get(1) + "e" + main.setColors().get(3) + "l" + main.setColors().get(4) + "l" + main.setColors().get(6) + "u");
@@ -57,11 +65,6 @@ public class SnowBallThrow implements Listener {
                                 e.setCancelled(true);
                                 ball.setFireTicks(1200);
                                 ball.setFallDistance(2000);
-                            } else if (p.getName().equals("Stellachen")) {
-                                main.shuffle();
-                                ball.setCustomName(main.setColors().get(2) + "S" + main.setColors().get(1) + "t" + main.setColors().get(3) + "e" + main.setColors().get(4) + "l" + main.setColors().get(6) + "l" + main.setColors().get(7) + "a");
-                                ball.setCustomNameVisible(true);
-                                e.setCancelled(true);
                             } else {
                                 ball.setCustomName(p.getDisplayName());
                                 ball.setCustomNameVisible(true);
@@ -73,17 +76,9 @@ public class SnowBallThrow implements Listener {
 
                     }
                 } else {
-                    ItemStack d1 = new ItemStack(Material.SNOWBALL, 94);
-                    ItemMeta d2 = d1.getItemMeta();
-                    d2.setDisplayName(main.hubitem5);
-                    d2.setLore(Arrays.asList(main.hubitem5_info_1, main.hubitem5_info_2, main.hubitem5_info_3, main.hubitem5_info_4));
-                    d1.setItemMeta(d2);
-                    p.getInventory().setItem(2, new ItemStack(d1));
+                    p.getInventory().setItem(2, new ItemStack(snowballs));
                 }
-            } else {
             }
-
-        } else {
         }
-    }   
+    }
 }
