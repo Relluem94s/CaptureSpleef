fb787460d80ffe9d4bf7edc400117713b2349cc2
Rellu <github@relluem94.de>
2020-12-19 03:03:47 +0100

    clean up

--- src/de/relluem94/capturespleef/CmdEv.java
+++ src/de/relluem94/capturespleef/CmdEv.java
@@ -20,13 +20,17 @@ public class CmdEv {
     public void registerEvents() {
         PluginManager pm = main.getServer().getPluginManager();
         main.cSM(main.prefix, "§aRegistriere Event!");
+        //TODO Remove mains from constructor
         pm.registerEvents(new de.relluem94.capturespleef.listener.BlockDamage(main), main);
         pm.registerEvents(new de.relluem94.capturespleef.listener.EntityDamageByEntity(main), main);
         pm.registerEvents(new de.relluem94.capturespleef.listener.PlayerCommandPreprocess(main), main);
         pm.registerEvents(new de.relluem94.capturespleef.listener.PlayerInteract(main), main);
         pm.registerEvents(new de.relluem94.capturespleef.listener.PlayerMove(main), main);
-        pm.registerEvents(new de.relluem94.capturespleef.listener.PlayerQuit(main), main);
+        pm.registerEvents(new de.relluem94.capturespleef.events.PlayerQuit(main), main);
         pm.registerEvents(new de.relluem94.capturespleef.events.SignChange(main), main);
+        pm.registerEvents(new de.relluem94.capturespleef.events.GameJoin(main), main);
+        pm.registerEvents(new de.relluem94.capturespleef.events.SignUse(), main);
+        pm.registerEvents(new de.relluem94.capturespleef.events.SnowBall(main), main);
         main.cSM(main.prefix, "§aEvents erfolgreich regestriert!");
     }
 
