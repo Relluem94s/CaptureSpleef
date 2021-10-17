7088b4263ec64bb50dbc4be9c1e29b697ca00daf
Relluem94 <github@relluem94.de>
2021-10-17 02:47:55 +0200

    rewrite

--- src/main/java/de/relluem94/capturespleef/events/SignUse.java
+++ src/main/java/de/relluem94/capturespleef/events/SignUse.java
@@ -1,7 +1,17 @@
 package de.relluem94.capturespleef.events;
 
+import static de.relluem94.capturespleef.Strings.COMMAND_CASP;
+import static de.relluem94.capturespleef.Strings.COMMAND_CASP_JOIN;
+import static de.relluem94.capturespleef.Strings.COMMAND_CASP_LEAVE;
+import static de.relluem94.capturespleef.Strings.JOIN_GAME;
+import static de.relluem94.capturespleef.Strings.LEFT_GAME;
+import static de.relluem94.capturespleef.Strings.PLUGIN_PREFIX;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
+import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
 import de.relluem94.rellulib.utils.LogUtils;
+import java.util.Objects;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
@@ -18,26 +28,21 @@ public class SignUse implements Listener {
 
     @EventHandler
     public void useSign(PlayerInteractEvent e) {
+        final Player p = e.getPlayer();
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.LEFT_CLICK_BLOCK) {
-            if (e.getClickedBlock().getType().equals(Material.OAK_WALL_SIGN)
-                    || e.getClickedBlock().getType().equals(Material.OAK_SIGN)
-                    || e.getPlayer().hasPermission("rellu.sign.item.use")) {
+            Block block = Objects.requireNonNull(e.getClickedBlock());
+            Material material = block.getType();
+            if (material.equals(Material.OAK_WALL_SIGN) || material.equals(Material.OAK_SIGN) || Permission.isAuthorized(p, Groups.getGroup("user").getId())) {
                 try {
-                    BlockState schild = e.getClickedBlock().getState();
-
-                    final Player p = e.getPlayer();
-                   
-                    if (p.hasPermission("rellu.capturespleef.sign.use")) {
-
-                        if (((schild instanceof Sign)) && (e.getAction() == Action.RIGHT_CLICK_BLOCK)) {
-                            Sign s = (Sign) e.getClickedBlock().getState();
-                            if ((s.getLine(0).equalsIgnoreCase("§d[CaptureSpleef]")) && (s.getLine(2).equalsIgnoreCase("§aSpiel verlassen"))) {
-                                p.performCommand("casp leave");
-                            } else if ((s.getLine(0).equalsIgnoreCase("§d[CaptureSpleef]")) && (s.getLine(2).equalsIgnoreCase("§aSpiel betreten"))) {
-                                p.performCommand("casp join");
+                    BlockState bs = block.getState();                   
+                     if (((bs instanceof Sign)) && (e.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+                            Sign s = (Sign) bs;
+                            if ((s.getLine(0).equalsIgnoreCase(PLUGIN_PREFIX)) && (s.getLine(2).equalsIgnoreCase(LEFT_GAME))) {
+                                p.performCommand(COMMAND_CASP + " " + COMMAND_CASP_LEAVE);
+                            } else if ((s.getLine(0).equalsIgnoreCase(PLUGIN_PREFIX)) && (s.getLine(2).equalsIgnoreCase(JOIN_GAME))) {
+                                p.performCommand(COMMAND_CASP + " " + COMMAND_CASP_JOIN);
                             }
                         }
-                    }
                 } catch (IndexOutOfBoundsException ex) {
                     LogUtils.error(ex.getMessage());
                 }
