56926259f9683b645c961c96695ac4fee9b9b276
Relluem94 <github@relluem94.de>
2021-10-17 02:48:32 +0200

    added some strings and fixed static string use for prefix...

--- src/main/java/de/relluem94/capturespleef/Strings.java
+++ src/main/java/de/relluem94/capturespleef/Strings.java
@@ -6,23 +6,30 @@ package de.relluem94.capturespleef;
  */
 public class Strings {
 
+    public final static String CS_NAME = "CaptureSpleef";
+    public final static String PLUGIN_PREFIX = "§d[" + CS_NAME + "]";
+    
     public final static String TEAM_RED_NAME = "TeamRot";
     public final static String TEAM_BLUE_NAME = "TeamBlau";
 
-    public final static String PLUGIN_PREFIX = "§d[" + Strings.CS_NAME + "]";
     public final static String ACTIVE_WORLD = "lobby";
 
     public final static String PLUGIN_COMMAND_COLOR = "§a";
 
-    public final static String CS_NAME = "CaptureSpleef";
-
     public final static String NO_USER = PLUGIN_COMMAND_COLOR + "Du bist kein Spieler";
     public final static String NO_PERM = PLUGIN_COMMAND_COLOR + "Du hast darauf keine Rechte";
+    
+    public static final String LEFT_GAME = PLUGIN_COMMAND_COLOR + "Spiel verlassen";
+    public static final String JOIN_GAME = PLUGIN_COMMAND_COLOR + "Spiel betreten";
 
     public final static String REGISTER_COMMANDS = PLUGIN_COMMAND_COLOR + "Befehle werden regestriert!";
     public final static String REGISTER_COMMANDS_FINISHED = PLUGIN_COMMAND_COLOR + "Befehle erfolgreich regestriert!";
     public final static String REGISTER_EVENTS = PLUGIN_COMMAND_COLOR + "Registriere Events!";
     public final static String REGISTER_EVENTS_FINISHED = PLUGIN_COMMAND_COLOR + "Events erfolgreich regestriert!";
+    
+    public final static String COMMAND_CASP = "casp";
+    public final static String COMMAND_CASP_JOIN = "join";
+    public final static String COMMAND_CASP_LEAVE = "leave";
 
     public final static String SIGN_CREATE = PLUGIN_COMMAND_COLOR + "Schild erstellt";
 
