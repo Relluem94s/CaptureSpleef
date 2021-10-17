c02ce16f16bdec069a01c4890c2bbe5f9e25ec10
Relluem94 <github@relluem94.de>
2021-10-17 03:56:46 +0200

    removed main from event + little rewrite

--- src/main/java/de/relluem94/capturespleef/events/PlayerQuit.java
+++ src/main/java/de/relluem94/capturespleef/events/PlayerQuit.java
@@ -1,5 +1,10 @@
 package de.relluem94.capturespleef.events;
 
+import static de.relluem94.capturespleef.CaptureSpleef.blau;
+import static de.relluem94.capturespleef.CaptureSpleef.cslobby;
+import static de.relluem94.capturespleef.CaptureSpleef.rot;
+import static de.relluem94.capturespleef.CaptureSpleef.sboard;
+import static de.relluem94.capturespleef.CaptureSpleef.teams;
 import static de.relluem94.capturespleef.Strings.CS_NAME;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -8,36 +13,36 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import static de.relluem94.capturespleef.Strings.TEAM_RED_NAME;
 import static de.relluem94.capturespleef.Strings.TEAM_BLUE_NAME;
+import java.util.Objects;
 
 public class PlayerQuit implements Listener {
 
-    de.relluem94.capturespleef.CaptureSpleef main;
-
-    public PlayerQuit(de.relluem94.capturespleef.CaptureSpleef instance) {
-        main = instance;
-    }
-
-    @SuppressWarnings("deprecation")
     @EventHandler
     public void GameIntrupt(PlayerQuitEvent e) {
         Player p = e.getPlayer();
-        if (p.getCustomName().equals(TEAM_RED_NAME)) {
-            main.teams.get(p).getBlock().setType(Material.AIR);
-            main.teams.get(p).getBlock().getRelative(0, -1, 0).setType(Material.NETHER_BRICK);
-            main.teams.remove(p);
-            main.rot.removePlayer(p);
-            main.sboard.resetScores(p);
-        } else if (p.getCustomName().equals(TEAM_BLUE_NAME)) {
-            main.teams.get(p).getBlock().setType(Material.AIR);
-            main.teams.get(p).getBlock().getRelative(0, -1, 0).setType(Material.PRISMARINE);
-            main.teams.remove(p);
-            main.blau.removePlayer(p);
-            main.sboard.resetScores(p);
-        } else if (p.getCustomName().equals(CS_NAME)) {
-            main.cslobby.removePlayer(p);
-            main.sboard.resetScores(p);
+        String name = Objects.requireNonNull(p.getCustomName());
+        
+        switch (name) {
+            case TEAM_RED_NAME:
+                teams.get(p).getBlock().setType(Material.AIR);
+                teams.get(p).getBlock().getRelative(0, -1, 0).setType(Material.NETHER_BRICK);
+                teams.remove(p);
+                rot.removePlayer(p);
+                sboard.resetScores(p);
+                break;
+            case TEAM_BLUE_NAME:
+                teams.get(p).getBlock().setType(Material.AIR);
+                teams.get(p).getBlock().getRelative(0, -1, 0).setType(Material.PRISMARINE);
+                teams.remove(p);
+                blau.removePlayer(p);
+                sboard.resetScores(p);
+                break;
+            case CS_NAME:
+                cslobby.removePlayer(p);
+                sboard.resetScores(p);
+                break;
+            default:
+                break;
         }
-
     }
-
 }
