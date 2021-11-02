498ada67308874cc2d2e31c131cc416a04a68c7e
Relluem94 <github@relluem94.de>
2021-11-02 22:21:37 +0100

    changed colors to match design pattern removed command and event register commands as it is now imported from relluEssentials

--- src/main/java/de/relluem94/capturespleef/Strings.java
+++ src/main/java/de/relluem94/capturespleef/Strings.java
@@ -9,7 +9,7 @@ public class Strings {
     public final static String CS_NAME = "CaptureSpleef";
     public final static String PLUGIN_PRIMARY_COLOR = "§d"; 
     public final static String PLUGIN_SECONDARY_COLOR = "§a";
-    public final static String PLUGIN_PREFIX = PLUGIN_PRIMARY_COLOR + "[" + CS_NAME + "]";
+    public final static String PLUGIN_PREFIX = PLUGIN_SECONDARY_COLOR + "[" + PLUGIN_PRIMARY_COLOR + CS_NAME + PLUGIN_SECONDARY_COLOR + "]";
     
     public final static String TEAM_RED_NAME = "TeamRot";
     public final static String TEAM_BLUE_NAME = "TeamBlau";
@@ -21,11 +21,6 @@ public class Strings {
     
     public static final String LEFT_GAME = PLUGIN_SECONDARY_COLOR + "Spiel verlassen";
     public static final String JOIN_GAME = PLUGIN_SECONDARY_COLOR + "Spiel betreten";
-
-    public final static String REGISTER_COMMANDS = PLUGIN_SECONDARY_COLOR + "Befehle werden regestriert!";
-    public final static String REGISTER_COMMANDS_FINISHED = PLUGIN_SECONDARY_COLOR + "Befehle erfolgreich regestriert!";
-    public final static String REGISTER_EVENTS = PLUGIN_SECONDARY_COLOR + "Registriere Events!";
-    public final static String REGISTER_EVENTS_FINISHED = PLUGIN_SECONDARY_COLOR + "Events erfolgreich regestriert!";
     
     public final static String COMMAND_CASP = "casp";
     public final static String COMMAND_CASP_JOIN = "join";
