cd927ef1a068bd49540eeb97b9fee3e3a0a40c5d
Relluem94 <github@relluem94.de>
2021-11-07 20:27:12 +0100

    format

--- src/main/java/de/relluem94/capturespleef/Strings.java
+++ src/main/java/de/relluem94/capturespleef/Strings.java
@@ -7,22 +7,22 @@ package de.relluem94.capturespleef;
 public class Strings {
 
     public final static String CS_NAME = "CaptureSpleef";
-    public final static String PLUGIN_PRIMARY_COLOR = "§d"; 
+    public final static String PLUGIN_PRIMARY_COLOR = "§d";
     public final static String PLUGIN_SECONDARY_COLOR = "§a";
     public final static String PLUGIN_NAME_CONSOLE = PLUGIN_SECONDARY_COLOR + "[" + PLUGIN_PRIMARY_COLOR + CS_NAME + PLUGIN_SECONDARY_COLOR + "]";
     public final static String PLUGIN_PREFIX = PLUGIN_PRIMARY_COLOR + CS_NAME;
-    
+
     public final static String TEAM_RED_NAME = "TeamRot";
     public final static String TEAM_BLUE_NAME = "TeamBlau";
 
-    public final static String ACTIVE_WORLD = "lobby";    
+    public final static String ACTIVE_WORLD = "lobby";
 
     public final static String NO_USER = PLUGIN_SECONDARY_COLOR + "Du bist kein Spieler";
     public final static String NO_PERM = PLUGIN_SECONDARY_COLOR + "Du hast darauf keine Rechte";
-    
+
     public static final String LEFT_GAME = PLUGIN_SECONDARY_COLOR + "Spiel verlassen";
     public static final String JOIN_GAME = PLUGIN_SECONDARY_COLOR + "Spiel betreten";
-    
+
     public final static String COMMAND_CASP = "casp";
     public final static String COMMAND_CASP_JOIN = "join";
     public final static String COMMAND_CASP_LEAVE = "leave";
