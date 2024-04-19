9116a1cec632b49f3776a0f9708fbc279ea9bf54
Relluem94 <github@relluem94.de>
2024-04-19 23:38:06 +0200

    CS-9 Translate

--- src/main/java/de/relluem94/capturespleef/Strings.java
+++ src/main/java/de/relluem94/capturespleef/Strings.java
@@ -12,26 +12,26 @@ public class Strings {
     public final static String PLUGIN_NAME_CONSOLE = PLUGIN_SECONDARY_COLOR + "[" + PLUGIN_PRIMARY_COLOR + CS_NAME + PLUGIN_SECONDARY_COLOR + "]";
     public final static String PLUGIN_PREFIX = PLUGIN_PRIMARY_COLOR + CS_NAME;
 
-    public final static String TEAM_RED_NAME = "TeamRot";
-    public final static String TEAM_BLUE_NAME = "TeamBlau";
+    public final static String TEAM_RED_NAME = "TeamRed";
+    public final static String TEAM_BLUE_NAME = "TeamBlue";
 
     public final static String ACTIVE_WORLD = "lobby";
 
-    public final static String NO_USER = PLUGIN_SECONDARY_COLOR + "Du bist kein Spieler";
-    public final static String NO_PERM = PLUGIN_SECONDARY_COLOR + "Du hast darauf keine Rechte";
+    public final static String NO_USER = PLUGIN_SECONDARY_COLOR + "You are not a player";
+    public final static String NO_PERM = PLUGIN_SECONDARY_COLOR + "You have no rights to do that";
 
-    public static final String LEFT_GAME = PLUGIN_SECONDARY_COLOR + "Spiel verlassen";
-    public static final String JOIN_GAME = PLUGIN_SECONDARY_COLOR + "Spiel betreten";
+    public static final String LEFT_GAME = PLUGIN_SECONDARY_COLOR + "Exit game";
+    public static final String JOIN_GAME = PLUGIN_SECONDARY_COLOR + "Enter game";
 
     public final static String COMMAND_CASP = "casp";
     public final static String COMMAND_CASP_JOIN = "join";
     public final static String COMMAND_CASP_LEAVE = "leave";
 
-    public final static String SIGN_CREATE = PLUGIN_SECONDARY_COLOR + "Schild erstellt";
+    public final static String SIGN_CREATE = PLUGIN_SECONDARY_COLOR + "Sign created";
 
-    public final static String SNOWBALL_DISPLAYNAME = "§fSchneeball";
+    public final static String SNOWBALL_DISPLAYNAME = "§fSnowball";
 
-    public final static String[] SNOWBALL_META_INFO = {"Dieser Schneeball dient dir als Hilfsmittel", "", "", ""};
+    public final static String[] SNOWBALL_META_INFO = {"This snowball serves as a tool for you", "", "", ""};
 
     public final static String PLUGIN_CHAT_SPACER = "▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀▶◀";
 
